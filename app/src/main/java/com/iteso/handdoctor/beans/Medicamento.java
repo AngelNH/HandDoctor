f8589cd1155a9d9e7dab2dea622b1445fef8c830
VickyVillarreal <vickypop95@gmail.com>
2018-04-25 00:49:42 -0500

    Update Medicamento.java

--- app/src/main/java/com/iteso/handdoctor/beans/Medicamento.java
+++ app/src/main/java/com/iteso/handdoctor/beans/Medicamento.java
@@ -2,59 +2,89 @@ package com.iteso.handdoctor.beans;
 
 
 public class Medicamento {
-    private String name;
-    private int mg;
-    private int cantidad;
-    private int dias;
+    private String idMed;
+    private String cantidad;
+    private int idPaciente;
+    private int diasRestantes;
+    private String dosis, nombre;
+    private int tipo;
 
-    public Medicamento(String name, int cantidad, int mg, int dias) {
-        this.name = name;
+    public Medicamento(String idMed, String cantidad, int idPaciente, int diasRestantes, String dosis, String nombre, int tipo) {
+        this.idMed = idMed;
         this.cantidad = cantidad;
-        this.mg = mg;
-        this.dias = dias;
+        this.idPaciente = idPaciente;
+        this.diasRestantes = diasRestantes;
+        this.dosis = dosis;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public String getIdMed() {
+        return idMed;
+    }
+
+    public void setIdMed(String idMed) {
+        this.idMed = idMed;
+    }
+
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(String cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public int getDiasRestantes() {
+        return diasRestantes;
+    }
+
+    public void setDiasRestantes(int diasRestantes) {
+        this.diasRestantes = diasRestantes;
+    }
+
+    public String getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(String dosis) {
+        this.dosis = dosis;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
     }
 
     @Override
     public String toString() {
         return "Medicamento{" +
-                "name='" + name + '\'' +
-                ", cantidad=" + cantidad +
-                ", mg='" + mg + '\'' +
-                ", dias=" + dias +
+                "idMed='" + idMed + '\'' +
+                ", cantidad='" + cantidad + '\'' +
+                ", idPaciente=" + idPaciente +
+                ", diasRestantes=" + diasRestantes +
+                ", dosis='" + dosis + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", tipo=" + tipo +
                 '}';
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    public int getMg() {
-        return mg;
-    }
-
-    public void setMg(int mg) {
-        this.mg = mg;
-    }
-
-    public int getDias() {
-        return dias;
-    }
-
-    public void setDias(int dias) {
-        this.dias = dias;
-    }
-
-
 }

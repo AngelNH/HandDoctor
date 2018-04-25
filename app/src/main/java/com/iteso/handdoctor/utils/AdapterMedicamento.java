5edcfbcd4a5f5d92e05b739052228801a005e405
VickyVillarreal <vickypop95@gmail.com>
2018-04-25 00:51:16 -0500

    Update AdapterMedicamento.java

--- app/src/main/java/com/iteso/handdoctor/utils/AdapterMedicamento.java
+++ app/src/main/java/com/iteso/handdoctor/utils/AdapterMedicamento.java
@@ -7,19 +7,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+
 import com.iteso.handdoctor.R;
 import com.iteso.handdoctor.beans.Medicamento;
+
 import java.util.ArrayList;
 
 public class AdapterMedicamento extends BaseAdapter {
 
     protected Activity activity;
     protected ArrayList<Medicamento> medicamentos;
-
-    private TextView name;
-    private TextView miligramos;
+    private TextView idMed;
     private TextView cantidad;
-    private TextView dias;
+    private TextView idPaciente;
+    private TextView diasRestantes;
+    private TextView dosis, nombre;
+    private TextView tipo;
 
     public AdapterMedicamento(Activity activity, ArrayList<Medicamento> medicamentos) {
         this.activity = activity;
@@ -50,6 +53,8 @@ public class AdapterMedicamento extends BaseAdapter {
     }
 
 
+
+
     @Override
     public View getView(int pos, View view, ViewGroup viewGroup) {
         View v = view;
@@ -58,15 +63,15 @@ public class AdapterMedicamento extends BaseAdapter {
             v = inflater.inflate(R.layout.medicamentos_items,null);
         }
 
-        Medicamento med = medicamentos.get(pos);
-        name = v.findViewById(R.id.medicamento_label);
-        name.setText(med.getName());
-        miligramos = v.findViewById(R.id.miligramos_label);
-        miligramos.setText(med.getMg());
-        cantidad=v.findViewById(R.id.cantidad_label);
+         Medicamento med = medicamentos.get(pos);
+        nombre = v.findViewById(R.id.nombreMedicamento_label);
+        nombre.setText(med.getNombre());
+        cantidad = v.findViewById(R.id.cantidad_label);
         cantidad.setText(med.getCantidad());
-        dias=v.findViewById(R.id.dias_label);
-        dias.setText(med.getDias());
+        dosis=v.findViewById(R.id.dosis_label);
+        dosis.setText(med.getDosis());
+        diasRestantes=v.findViewById(R.id.diasRestantes_label);
+        diasRestantes.setText(med.getDiasRestantes());
         return v;
     }
 }

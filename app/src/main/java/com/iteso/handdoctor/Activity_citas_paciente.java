4541fb4f4704fdc2ff688d1f6fc0709469b7feb6
VickyVillarreal <vickypop95@gmail.com>
2018-04-25 00:53:17 -0500

    Update Activity_citas_paciente.java

--- app/src/main/java/com/iteso/handdoctor/Activity_citas_paciente.java
+++ app/src/main/java/com/iteso/handdoctor/Activity_citas_paciente.java
@@ -8,7 +8,9 @@ import android.widget.ListView;
 
 import com.iteso.handdoctor.beans.CitasPaciente;
 import com.iteso.handdoctor.utils.AdapterCitasPacientes;
+
 import java.util.ArrayList;
+
 public class Activity_citas_paciente extends AppCompatActivity {
     ListView listView;
     @Override
@@ -17,13 +19,13 @@ public class Activity_citas_paciente extends AppCompatActivity {
         setContentView(R.layout.activity_citas_paciente);
         listView = findViewById(R.id.citaspaciente_list_view);
         ArrayList<CitasPaciente> cp = new ArrayList<>();
-        cp.add(new CitasPaciente("Vicky", "12 de mayo 2018", "Plaza del sol", "Medico general", "3 pm"));
-        cp.add(new CitasPaciente("Jorge", "1 de mayo 2018", "iteso", "Psicologa", "6 pm"));
-        cp.add(new CitasPaciente("Hector", "29 de abril 2018", "Galerias", "Dermatologo", "11 am"));
-        cp.add(new CitasPaciente("Luis", "28 de junio 2018", "Centro magno", "Ginecologo", "4 pm"));
-        cp.add(new CitasPaciente("Yara", "31 de julio 2018", "Plaza del sol", "Medico general", "7 pm"));
-        cp.add(new CitasPaciente("Miguel", "12 de mayo 2018", "Plaza del sol" , "Medico general", "8 am"));
-        cp.add(new CitasPaciente("Mariana", "13 de mayo 2018", "Plaza del sol", "Medico general", "9 pm"));
+        cp.add(new CitasPaciente("Vicky", "12 de mayo 2018", 200, 1569, "Medico general", "3 pm"));
+        cp.add(new CitasPaciente("Jorge", "1 de mayo 2018", 200, 1569,  "Psicologa", "6 pm"));
+        cp.add(new CitasPaciente("Hector", "29 de abril 2018", 200, 1569,  "Dermatologo", "11 am"));
+        cp.add(new CitasPaciente("Luis", "28 de junio 2018",200, 1569,  "Ginecologo", "4 pm"));
+        cp.add(new CitasPaciente("Yara", "31 de julio 2018", 200, 1569, "Medico general", "7 pm"));
+        cp.add(new CitasPaciente("Miguel", "12 de mayo 2018", 200, 1569,  "Medico general", "8 am"));
+        cp.add(new CitasPaciente("Mariana", "13 de mayo 2018", 200, 1569,  "Medico general", "9 pm"));
 
         AdapterCitasPacientes ad = new AdapterCitasPacientes(this, cp);
         listView.setAdapter(ad);

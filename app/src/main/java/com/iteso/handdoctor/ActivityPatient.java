f3752cf27867b365a337a6a3373573fcf348f9df
Leonardo <is705310@iteso.mx>
2018-05-14 04:20:12 -0500

    Commit 4 am

--- app/src/main/java/com/iteso/handdoctor/ActivityPatient.java
+++ app/src/main/java/com/iteso/handdoctor/ActivityPatient.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,22 +15,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.ViewSwitcher.ViewFactory;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.iteso.handdoctor.beans.CitasPaciente;
+import com.iteso.handdoctor.utils.AdapterCitasPacientes;
 
 public class ActivityPatient extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private ImageSwitcher imageSwitcher;
     private int[] gallery = { R.drawable.advice, R.drawable.advice1, R.drawable.advice2,
             R.drawable.advice3, R.drawable.advice4};
+    String id;
+    AdapterCitasPacientes ad;
+    DatabaseReference databaseReference;
+    ListView listView;
+    ArrayList<CitasPaciente> cp = new ArrayList<>();
 
     private int position;
 
@@ -42,6 +59,54 @@ public class ActivityPatient extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient);
+        listView = findViewById(R.id.citaspaciente_list_view);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final int pos = position;
+            }
+        });
+        loadID();
+        ad= new AdapterCitasPacientes(this, cp);
+        id= "3318275490";
+        //Firebase
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.child("Paciente").child(id).child("Citas").getChildren()){
+                    CitasPaciente cita = data.getValue(CitasPaciente.class);
+                    cp.add(cita);
+                    Log.e("ACTIVITY_CITAS_PAC",cita.toString());
+                }
+                ad.notifyDataSetChanged();
+                listView.deferNotifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        listView.setAdapter(ad);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String lon = cp.get(position).getConsultorioLon();
+                String lat = cp.get(position).getConsultorioLat();
+                String name = cp.get(position).getName_doc();
+                Intent i = new Intent(ActivityPatient.this,ActivityMaps.class);
+                i.putExtra("LAT",lat);
+                i.putExtra("LON",lon);
+                i.putExtra("NAME",name);
+                startActivity(i);
+            }
+        });
+        //Log.e("FIREBASE",citas.toString());
 
 
         //VICKY
@@ -72,14 +137,14 @@ public class ActivityPatient extends AppCompatActivity
         Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -202,6 +267,13 @@ public class ActivityPatient extends AppCompatActivity
         editor.apply();
         Toast.makeText(this,"You just Logged out", Toast.LENGTH_SHORT).show();
         finish();
+    }
+
+    public void loadID(){
+        SharedPreferences sharedPreferences =
+                getSharedPreferences("com.iteso.HANDDOCTOR_PREFERENCES",MODE_PRIVATE);
+        id = sharedPreferences.getString("ID","555");
+        sharedPreferences = null;
     }
 
 
